7a299dff17b08d5564b8005c932dd884be527c33
Zevsandrey2010 <kochkarandrey2010@gmail.com>
2026-07-03 19:54:58 +0300

    Урок 1

--- tasks/views.py
+++ tasks/views.py
@@ -1,72 +1,101 @@
-from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from django.views.generic import (
-    ListView,
-    DetailView,
-    CreateView,
-    UpdateView,
-    DeleteView,
-)
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import redirect
 from django.urls import reverse_lazy
+from django.views.generic import CreateView, DetailView, ListView
+from django.views.generic.edit import DeleteView, UpdateView
 
-from .models import Task
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+
+from .forms import TaskForm, CommentForm
+from .mixins import PermissionDenied, UserIsOwnerMixin
+from .models import Task, Comment
 
 
-class TaskListView(LoginRequiredMixin, ListView):
+class TaskListView(ListView):
     model = Task
     template_name = "tasks/task_list.html"
     context_object_name = "tasks"
-    ordering = ["-created_at"]
 
 
-class TaskDetailView(LoginRequiredMixin, DetailView):
+class TaskDetailView(DetailView):
     model = Task
     template_name = "tasks/task_detail.html"
     context_object_name = "task"
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["comments"] = self.object.comments.all().order_by("-created_at")
+        context["form"] = CommentForm()
+        return context
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.author = request.user
+            comment.task = self.object
+            comment.save()
+        return redirect("tasks:task_detail", pk=self.object.pk)
+
 
 class TaskCreateView(LoginRequiredMixin, CreateView):
     model = Task
+    form_class = TaskForm
     template_name = "tasks/task_form.html"
-
-    fields = [
-        "title",
-        "pole",
-        "description",
-        "status",
-        "priority",
-        "due_date",
-        "file",
-    ]
+    success_url = reverse_lazy("tasks:task_list")
 
     def form_valid(self, form):
         form.instance.creator = self.request.user
         return super().form_valid(form)
 
 
-class TaskUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+class TaskUpdateView(LoginRequiredMixin, UserIsOwnerMixin, UpdateView):
     model = Task
+    form_class = TaskForm
     template_name = "tasks/task_form.html"
-
-    fields = [
-        "title",
-        "pole",
-        "description",
-        "status",
-        "priority",
-        "due_date",
-        "file",
-    ]
-
-    def test_func(self):
-        task = self.get_object()
-        return task.creator == self.request.user
+    success_url = reverse_lazy("tasks:task_list")
 
 
-class TaskDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+class TaskDeleteView(LoginRequiredMixin, UserIsOwnerMixin, DeleteView):
     model = Task
     template_name = "tasks/task_confirm_delete.html"
-    success_url = reverse_lazy("tasks:task-list")
+    success_url = reverse_lazy("tasks:task_list")
 
-    def test_func(self):
-        task = self.get_object()
-        return task.creator == self.request.user
+
+class RegisterView(CreateView):
+    template_name = "registration/register.html"
+    form_class = UserCreationForm
+    success_url = reverse_lazy("tasks:task_list")
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        login(self.request, self.object)
+        return response
+    
+
+class CommentEditView(LoginRequiredMixin, UpdateView):
+    model = Comment
+    form_class = CommentForm
+    template_name = "tasks/comment_form.html"
+
+    def get_success_url(self):
+        return reverse_lazy("tasks:task_detail", kwargs={"pk": self.object.task.pk})
+
+    def dispatch(self, request, *args, **kwargs):
+        if self.get_object().author != request.user:
+            raise PermissionDenied
+        return super().dispatch(request, *args, **kwargs)
+
+class CommentDeleteView(LoginRequiredMixin, DeleteView):
+    model = Comment
+    template_name = "tasks/comment_confirm_delete.html"
+
+    def get_success_url(self):
+        return reverse_lazy("tasks:task_detail", kwargs={"pk": self.object.task.pk})
+
+    def dispatch(self, request, *args, **kwargs):
+        if self.get_object().author != request.user:
+            raise PermissionDenied
+        return super().dispatch(request, *args, **kwargs)
